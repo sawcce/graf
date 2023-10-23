c2c1c30e496d79fcc8b433f47e27835de3ef98d2
alexis <sawcce@proton.me>
2023-10-23 22:58:56 +0200

    Basic fps counter

--- src/main.c
+++ src/main.c
@@ -4,15 +4,28 @@
 #include "sokol_app.h"
 #include "sokol_log.h"
 #include "sokol_glue.h"
+#include "sokol_time.h"
 
 #include "scene.h"
 #include "log.h"
 
 sg_pass_action pass_action;
+uint64_t current_time;
+uint64_t delta_time;
+float_t refresh_rate;
+
+char *window_title;
 
 void init(void)
 {
     log_info("Initiating setup");
+    stm_setup();
+
+    uint64_t time = stm_now();
+    log_debug("Set time to: %d", time);
+    current_time = time;
+
+    window_title = malloc(50 * sizeof(char));
 
     sg_setup(&(sg_desc){
         .context = sapp_sgcontext(),
@@ -28,6 +41,16 @@ void init(void)
 
 void frame(void)
 {
+    delta_time = stm_laptime(&current_time);
+    float_t new_refresh_rate = 1.0f / stm_sec(delta_time);
+
+    if ((int64_t)new_refresh_rate != (int64_t)refresh_rate)
+    {
+        refresh_rate = new_refresh_rate;
+        sprintf_s(window_title, 50 * sizeof(window_title), "DJI | FPS: %d", (int64_t)refresh_rate);
+        sapp_set_window_title(window_title);
+    }
+
     sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
     scene_draw();
     sg_end_pass();
@@ -36,6 +59,7 @@ void frame(void)
 
 void cleanup(void)
 {
+    free(window_title);
     sg_shutdown();
 }
 
